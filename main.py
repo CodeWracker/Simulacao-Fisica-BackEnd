ad0d26b094444ce7620fb307ba2fbefd4d21c63c
Rodrigo Ferraz <rodrigo.ferraz.souza@grad.ufsc.br>
2019-06-08 17:41:49 -0300

    Iniciei um projeto de API com o flask, esta servindo o json com id e posicao, iniciei o venv

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("a")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+pos = [
+    {
+        'id': 0,
+        'center[2]': [100, 100],
+        'radius': 2
+    },
+    {
+        'id':1,
+        'center[2]': [150, 150],
+        'radius': 2
+    }
+]
+
+@app.route('/')
+def home():
+    return jsonify(pos)
