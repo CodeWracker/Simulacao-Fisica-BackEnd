355040355ce6734ed06da0b4e5875e17c573c912
Rodrigo Ferraz Souza <rodrigo.ferra.souza@grad.ufsc.br>
2019-06-12 17:38:00 -0300

    adicionei a classe e o objeto

--- main.py
+++ main.py
@@ -1,50 +1,51 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-
+import json
+quantBalls = 4
 app = Flask(__name__)
 CORS(app)
 
+with open('db.json') as json_file:
+    infos = json.load(json_file)
+print(infos)
+
+
 cont = 0
-pos = [
-    {
-        'id': 0,
-        'center_x': 100,
-        'center_y': 100,
-        'radius': 20
-    },
-    {
-        'id': 1,
-        'center_x': 50,
-        'center_y': 150,
-        'radius': 20
-    }
-]
+
+class Canvas(object):
+    def __init__(self):
+        self.width = 1000
+        self.heigh = 800
+canvas = Canvas()
+teste= json.dumps(canvas.__dict__)
+print(teste)
+def obj_dict(obj):
+    return obj.__dict__
+
+class Ball(object):
+    def __init__(self, x,y,num):
+        self.radius = 5
+        self.id = num
+        self.centerX = x
+        self.centerY = y
+    def move(self):
+        self.centerX = self.centerX + 10
+bolas = []
+for i in range(quantBalls):
+    bolas.append(Ball(50,10*i,i))
 
 @app.route('/')
 def plus():
-    global cont
+    for i in range(quantBalls):
+        infos["ball"].append(json.dumps(bolas[i].__dict__))
+        teste = json.dumps(bolas, default=obj_dict)
+    for i in range(quantBalls):
+        bolas[i].move()
+    print(teste)
+    return jsonify(infos)
+    
+    
+    
+    
 
-    while True:
 
-        
-        cont = cont + 2
-        if(cont>900):
-            cont = 0
-        global pos
-        pos = [
-            {
-                'id': 0,
-                'center_x': 100 + cont,
-                'center_y': 100,
-                'radius': 20
-            },
-            {
-                'id': 1,
-                'center_x': 50 + cont,
-                'center_y': 150,
-                'radius': 20
-            }
-        ]
-        
-        
-        return jsonify(pos)
